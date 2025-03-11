5f427495a86914a486b16634b7cbdccb416f8811
tegh04 <43411745+tegh04@users.noreply.github.com>
2025-03-11 20:58:55 +1100

    Add files via upload

---
+++ solver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from tsp_python import solve_it
+import sys
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        file_location = sys.argv[1].strip()
+        with open(file_location, 'r') as input_data_file:
+            input_data = input_data_file.read()
+        print(solve_it(input_data))
+    else:
+        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/tsp_51_1)')
+
